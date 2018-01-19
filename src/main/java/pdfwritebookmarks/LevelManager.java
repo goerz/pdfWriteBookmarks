96aaaab2c57c2c0fb8e774dab183862e36f8a507
Stefan Birkner <mail@stefan-birkner.de>
2018-01-19 09:55:30 +0100

    Don't assign items that are not used
    Keep it simple.

--- src/main/java/pdfwritebookmarks/LevelManager.java
+++ src/main/java/pdfwritebookmarks/LevelManager.java
@@ -51,10 +51,10 @@ public class LevelManager {
     public PDOutlineItem register(PDOutlineItem item, int level){
         // adds a new bookmark to the manager at level, and returns the
         // parent of the new bookmark
-        Object garbage = null; // used to pop to nirvana
+
         //remove all items that are not an ancestor of the current item
         while (level < currentlevel + 1){
-            garbage = levelstack.pop();
+            levelstack.pop();
             currentlevel--;
         }
         PDOutlineItem parent = levelstack.empty() ? null : levelstack.peek();
